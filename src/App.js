2fa711bbfd766dd09ff1cbbeac517f62f0f6baa2
phuongthubui <166175579+phuongthubui@users.noreply.github.com>
2024-04-19 14:49:20 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
       <Globe
         polygonsData={globeJson.features}
         polygonCapColor={(geometry) => {
-          return geometry.properties.visited ? "#f78fa9" : "grey";
+          return geometry.properties.visited ? "pink" : "grey";
         }}
         polygonSideColor={(geometry) => {
-          return geometry.properties.visited ? "pink" : "#636363";
+          return geometry.properties.visited ? "#f78fa9" : "#636363";
         }}
         polygonStrokeColor={(geometry) => {
-          return geometry.properties.visited ? "#636363" : "pink";
+          return geometry.properties.visited ? "#636363" : "#f78fa9";
         }}
         polygonLabel={(geometry) => {
           return geometry.properties.name;
